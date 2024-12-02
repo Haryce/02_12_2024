c5cf374d6d71703a5dba8a55ee979525e7e74572
Haryce <148765923+Haryce@users.noreply.github.com>
2024-12-02 17:36:47 +0900

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int cub(int num) {
+int Cub(int num) {
     return num * num * num;
 }
 int main() {
@@ -8,6 +8,6 @@ int main() {
     int g;
     cout << "Введите число для возвращения в куб: \n";
     cin >> g;
-    cout << cub(g);
+    cout << Cub(g);
     return 0;
 }
