debeb02cceba8f37449ddaa823da3dd1fc7d5253
Haryce <148765923+Haryce@users.noreply.github.com>
2024-12-03 14:12:30 +0900

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int TR(int a) {
+    return a >= 0;
+}
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int f;
+    cout << "Введите число: \n"; 
+    cin >> f;
+    if (TR(f)) {
+        cout << "Значение положительное" << endl;
+    }
+    else {
+        cout << "Значение отрицательное" << endl;
+    }
+    return 0;
+}
